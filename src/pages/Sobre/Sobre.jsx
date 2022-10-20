b053bd8cb44b0e48296e2eab29f081d6a20cff0c
Gabriel Luna <lunagabri@gmail.com>
2022-10-20 10:54:42 -0300

    Complemento

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -3,7 +3,24 @@ import estilos from "./Sobre.module.css";
 const Sobre = () => {
   return (
     <section>
-      <h2 className={estilos.titulo_secao}>Sobre</h2>
+      <h2 className={estilos.titulo_secao}>Sobre nosso PetShop</h2>
+      <div id="sobre">
+        <h3>Missão</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, sint
+          quis est blanditiis voluptas dolorum et odit illum deleniti veritatis!
+        </p>
+        <h3>Visão</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, sint
+          quis est blanditiis voluptas dolorum et odit illum deleniti veritatis!
+        </p>
+        <h3>Valores</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, sint
+          quis est blanditiis voluptas dolorum et odit illum deleniti veritatis!
+        </p>
+      </div>
     </section>
   );
 };
