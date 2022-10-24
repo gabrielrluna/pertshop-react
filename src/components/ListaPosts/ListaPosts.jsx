34b060c41405ca100a5c02d818abd5f42b53ab3c
Gabriel Luna <lunagabri@gmail.com>
2022-10-24 11:54:42 -0300

    Update ListaPosts.jsx

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,29 +1,57 @@
+import { useState, useEffect } from "react"; //Hooks do React
 import servidorApi from "../../api/server-api";
 import estilos from "./ListaPosts.module.css";
 
 const ListaPosts = () => {
-  console.log(servidorApi);
+  // Iniciamos o state do componente com um array vazio para, posteriormente,
+  // preenchê-lo" com os dados vindos da API. Esta atribuição será feita
+  // com o auxílio do setPosts.
+  const [posts, setPosts] = useState([]);
 
-  const postsTemp = [];
+  // console.log(servidorApi + "/posts");
 
-  const getPosts = async () => {
-    try {
-      const resposta = await fetch(`${servidorApi}/posts`);
-      const dados = await resposta.json();
-      console.log(dados);
-    } catch (error) {
-      console.log("Deu ruim!" + error.message);
+  useEffect(() => {
+    // const getPosts = async () => {
+    async function getPosts() {
+      try {
+        const resposta = await fetch(`${servidorApi}/posts`);
+        const dados = await resposta.json();
+        setPosts(dados);
+      } catch (error) {
+        console.log("Deu ruim!" + error.message);
+      }
     }
-  };
+    getPosts();
+  }, []);
 
-  getPosts();
+  /*Sobre o UseEffect 
+Este hook visa permitir um maior controle sobre "efeitos colaterais" na execução do componente.
+Recebe dois parâmetros:
+
+ 1º: função callback com o que será executado
+ 2º: lista de dependências que indicarão ao useEffect quando ele deverá funcionar 
+
+ - Se não passar a lista (ou seja, deixar sem []), useEffect executará toda 
+vez que o componente for renderizado. Portanto, o callback se torna um loop infinito.
+
+- Se passar a lista vazia (ou seja, deixar o [] vazio), useEffect executará somente no
+momento que o componente é renderizado a primeira vez*/
+
+  // const teste = posts.map((post) => {
+  //   return post;
+  // });
+
+  // console.log(teste);
+
   return (
     <div className={estilos.lista_posts}>
-      {postsTemp.map(({ id, titulo, subtitulo }) => {
-        <article className={estilos.post} key={id}>
-          <h3>{titulo}</h3>
-          <p>{subtitulo}</p>
-        </article>;
+      {posts.map(({ id, titulo, subtitulo }) => {
+        return (
+          <article className={estilos.post} key={id}>
+            <h3>{titulo}</h3>
+            <p>{subtitulo}</p>
+          </article>
+        );
       })}
     </div>
   );
